981b6669557f4d216a0a0d536b60e300d1caa68f
dan <danv@cstructor.com>
2018-06-25 20:17:09 -0700

    Add tic-tac-toe exploration

---
+++ TicTacToe/Form1.Designer.cs
@@ -0,0 +1,263 @@
+﻿namespace TicTacToeApp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.fileMenuStrip = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuFileNew = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuFileExit = new System.Windows.Forms.ToolStripMenuItem();
+            this.uxStatusStrip = new System.Windows.Forms.StatusStrip();
+            this.uxToolStripStatusLabel = new System.Windows.Forms.ToolStripStatusLabel();
+            this.uxButton1 = new System.Windows.Forms.Label();
+            this.uxButton2 = new System.Windows.Forms.Label();
+            this.uxButton3 = new System.Windows.Forms.Label();
+            this.uxButton4 = new System.Windows.Forms.Label();
+            this.uxButton5 = new System.Windows.Forms.Label();
+            this.uxButton6 = new System.Windows.Forms.Label();
+            this.uxButton7 = new System.Windows.Forms.Label();
+            this.uxButton8 = new System.Windows.Forms.Label();
+            this.uxButton9 = new System.Windows.Forms.Label();
+            this.fileMenuStrip.SuspendLayout();
+            this.uxStatusStrip.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // fileMenuStrip
+            // 
+            this.fileMenuStrip.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem});
+            this.fileMenuStrip.Location = new System.Drawing.Point(0, 0);
+            this.fileMenuStrip.Name = "fileMenuStrip";
+            this.fileMenuStrip.Size = new System.Drawing.Size(212, 24);
+            this.fileMenuStrip.TabIndex = 0;
+            this.fileMenuStrip.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.menuFileNew,
+            this.menuFileExit});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
+            this.fileToolStripMenuItem.Text = "&File";
+            // 
+            // menuFileNew
+            // 
+            this.menuFileNew.Name = "menuFileNew";
+            this.menuFileNew.Size = new System.Drawing.Size(152, 22);
+            this.menuFileNew.Text = "&New";
+            this.menuFileNew.Click += new System.EventHandler(this.menuFileNew_Click);
+            // 
+            // menuFileExit
+            // 
+            this.menuFileExit.Name = "menuFileExit";
+            this.menuFileExit.Size = new System.Drawing.Size(152, 22);
+            this.menuFileExit.Text = "E&xit";
+            this.menuFileExit.Click += new System.EventHandler(this.menuFileExit_Click);
+            // 
+            // uxStatusStrip
+            // 
+            this.uxStatusStrip.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.uxToolStripStatusLabel});
+            this.uxStatusStrip.Location = new System.Drawing.Point(0, 245);
+            this.uxStatusStrip.Name = "uxStatusStrip";
+            this.uxStatusStrip.Size = new System.Drawing.Size(212, 22);
+            this.uxStatusStrip.TabIndex = 1;
+            // 
+            // uxToolStripStatusLabel
+            // 
+            this.uxToolStripStatusLabel.Name = "uxToolStripStatusLabel";
+            this.uxToolStripStatusLabel.Size = new System.Drawing.Size(197, 17);
+            this.uxToolStripStatusLabel.Spring = true;
+            // 
+            // uxButton1
+            // 
+            this.uxButton1.AutoSize = true;
+            this.uxButton1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.uxButton1.Font = new System.Drawing.Font("Microsoft Sans Serif", 26.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.uxButton1.Location = new System.Drawing.Point(12, 27);
+            this.uxButton1.MinimumSize = new System.Drawing.Size(60, 60);
+            this.uxButton1.Name = "uxButton1";
+            this.uxButton1.Size = new System.Drawing.Size(60, 60);
+            this.uxButton1.TabIndex = 11;
+            this.uxButton1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.uxButton1.Click += new System.EventHandler(this.uxButton_Click);
+            // 
+            // uxButton2
+            // 
+            this.uxButton2.AutoSize = true;
+            this.uxButton2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.uxButton2.Font = new System.Drawing.Font("Microsoft Sans Serif", 26.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.uxButton2.Location = new System.Drawing.Point(78, 27);
+            this.uxButton2.MinimumSize = new System.Drawing.Size(60, 60);
+            this.uxButton2.Name = "uxButton2";
+            this.uxButton2.Size = new System.Drawing.Size(60, 60);
+            this.uxButton2.TabIndex = 12;
+            this.uxButton2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.uxButton2.Click += new System.EventHandler(this.uxButton_Click);
+            // 
+            // uxButton3
+            // 
+            this.uxButton3.AutoSize = true;
+            this.uxButton3.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.uxButton3.Font = new System.Drawing.Font("Microsoft Sans Serif", 26.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.uxButton3.Location = new System.Drawing.Point(144, 27);
+            this.uxButton3.MinimumSize = new System.Drawing.Size(60, 60);
+            this.uxButton3.Name = "uxButton3";
+            this.uxButton3.Size = new System.Drawing.Size(60, 60);
+            this.uxButton3.TabIndex = 13;
+            this.uxButton3.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.uxButton3.Click += new System.EventHandler(this.uxButton_Click);
+            // 
+            // uxButton4
+            // 
+            this.uxButton4.AutoSize = true;
+            this.uxButton4.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.uxButton4.Font = new System.Drawing.Font("Microsoft Sans Serif", 26.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.uxButton4.Location = new System.Drawing.Point(12, 96);
+            this.uxButton4.MinimumSize = new System.Drawing.Size(60, 60);
+            this.uxButton4.Name = "uxButton4";
+            this.uxButton4.Size = new System.Drawing.Size(60, 60);
+            this.uxButton4.TabIndex = 14;
+            this.uxButton4.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.uxButton4.Click += new System.EventHandler(this.uxButton_Click);
+            // 
+            // uxButton5
+            // 
+            this.uxButton5.AutoSize = true;
+            this.uxButton5.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.uxButton5.Font = new System.Drawing.Font("Microsoft Sans Serif", 26.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.uxButton5.Location = new System.Drawing.Point(78, 96);
+            this.uxButton5.MinimumSize = new System.Drawing.Size(60, 60);
+            this.uxButton5.Name = "uxButton5";
+            this.uxButton5.Size = new System.Drawing.Size(60, 60);
+            this.uxButton5.TabIndex = 15;
+            this.uxButton5.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.uxButton5.Click += new System.EventHandler(this.uxButton_Click);
+            // 
+            // uxButton6
+            // 
+            this.uxButton6.AutoSize = true;
+            this.uxButton6.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.uxButton6.Font = new System.Drawing.Font("Microsoft Sans Serif", 26.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.uxButton6.Location = new System.Drawing.Point(144, 96);
+            this.uxButton6.MinimumSize = new System.Drawing.Size(60, 60);
+            this.uxButton6.Name = "uxButton6";
+            this.uxButton6.Size = new System.Drawing.Size(60, 60);
+            this.uxButton6.TabIndex = 16;
+            this.uxButton6.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.uxButton6.Click += new System.EventHandler(this.uxButton_Click);
+            // 
+            // uxButton7
+            // 
+            this.uxButton7.AutoSize = true;
+            this.uxButton7.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.uxButton7.Font = new System.Drawing.Font("Microsoft Sans Serif", 26.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.uxButton7.Location = new System.Drawing.Point(12, 166);
+            this.uxButton7.MinimumSize = new System.Drawing.Size(60, 60);
+            this.uxButton7.Name = "uxButton7";
+            this.uxButton7.Size = new System.Drawing.Size(60, 60);
+            this.uxButton7.TabIndex = 17;
+            this.uxButton7.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.uxButton7.Click += new System.EventHandler(this.uxButton_Click);
+            // 
+            // uxButton8
+            // 
+            this.uxButton8.AutoSize = true;
+            this.uxButton8.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.uxButton8.Font = new System.Drawing.Font("Microsoft Sans Serif", 26.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.uxButton8.Location = new System.Drawing.Point(78, 166);
+            this.uxButton8.MinimumSize = new System.Drawing.Size(60, 60);
+            this.uxButton8.Name = "uxButton8";
+            this.uxButton8.Size = new System.Drawing.Size(60, 60);
+            this.uxButton8.TabIndex = 18;
+            this.uxButton8.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.uxButton8.Click += new System.EventHandler(this.uxButton_Click);
+            // 
+            // uxButton9
+            // 
+            this.uxButton9.AutoSize = true;
+            this.uxButton9.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.uxButton9.Font = new System.Drawing.Font("Microsoft Sans Serif", 26.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.uxButton9.Location = new System.Drawing.Point(144, 166);
+            this.uxButton9.MinimumSize = new System.Drawing.Size(60, 60);
+            this.uxButton9.Name = "uxButton9";
+            this.uxButton9.Size = new System.Drawing.Size(60, 60);
+            this.uxButton9.TabIndex = 19;
+            this.uxButton9.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.uxButton9.Click += new System.EventHandler(this.uxButton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(212, 267);
+            this.Controls.Add(this.uxButton9);
+            this.Controls.Add(this.uxButton8);
+            this.Controls.Add(this.uxButton7);
+            this.Controls.Add(this.uxButton6);
+            this.Controls.Add(this.uxButton5);
+            this.Controls.Add(this.uxButton4);
+            this.Controls.Add(this.uxButton3);
+            this.Controls.Add(this.uxButton2);
+            this.Controls.Add(this.uxButton1);
+            this.Controls.Add(this.uxStatusStrip);
+            this.Controls.Add(this.fileMenuStrip);
+            this.MainMenuStrip = this.fileMenuStrip;
+            this.Name = "Form1";
+            this.SizeGripStyle = System.Windows.Forms.SizeGripStyle.Hide;
+            this.Text = "Tic-Tac-Toe";
+            this.fileMenuStrip.ResumeLayout(false);
+            this.fileMenuStrip.PerformLayout();
+            this.uxStatusStrip.ResumeLayout(false);
+            this.uxStatusStrip.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip fileMenuStrip;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem menuFileNew;
+        private System.Windows.Forms.ToolStripMenuItem menuFileExit;
+        private System.Windows.Forms.StatusStrip uxStatusStrip;
+        private System.Windows.Forms.ToolStripStatusLabel uxToolStripStatusLabel;
+        private System.Windows.Forms.Label uxButton1;
+        private System.Windows.Forms.Label uxButton2;
+        private System.Windows.Forms.Label uxButton3;
+        private System.Windows.Forms.Label uxButton4;
+        private System.Windows.Forms.Label uxButton5;
+        private System.Windows.Forms.Label uxButton6;
+        private System.Windows.Forms.Label uxButton7;
+        private System.Windows.Forms.Label uxButton8;
+        private System.Windows.Forms.Label uxButton9;
+    }
+}
+
---
+++ TicTacToe/Form1.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace TicTacToeApp
+{
+    public partial class Form1 : Form
+    {
+        // Keep track of whose turn it is (X, O)
+        private string currentTurn;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            ResetBoard();
+        }
+
+        private void ResetBoard()
+        {
+            // Set the first turn to X
+            currentTurn = "X";
+
+            // Update the status bar
+            WhoseTurn();
+
+            // Reset the buttons to empty text
+            uxButton1.ResetText();
+            uxButton2.ResetText();
+            uxButton3.ResetText();
+            uxButton4.ResetText();
+            uxButton5.ResetText();
+            uxButton6.ResetText();
+            uxButton7.ResetText();
+            uxButton8.ResetText();
+            uxButton9.ResetText();
+
+            // Enable each button to be clickable
+            uxButton1.Enabled = true;
+            uxButton2.Enabled = true;
+            uxButton3.Enabled = true;
+            uxButton4.Enabled = true;
+            uxButton5.Enabled = true;
+            uxButton6.Enabled = true;
+            uxButton7.Enabled = true;
+            uxButton8.Enabled = true;
+            uxButton9.Enabled = true;
+        }
+
+        // Disable all buttons to not be clickable
+        private void DisableBoard()
+        {
+            uxButton1.Enabled = false;
+            uxButton2.Enabled = false;
+            uxButton3.Enabled = false;
+            uxButton4.Enabled = false;
+            uxButton5.Enabled = false;
+            uxButton6.Enabled = false;
+            uxButton7.Enabled = false;
+            uxButton8.Enabled = false;
+            uxButton9.Enabled = false;
+        }
+
+        // Update the status bar
+        private void WhoseTurn()
+        {
+            uxToolStripStatusLabel.Text = string.Format("{0}'s turn", currentTurn);
+        }
+
+        // Check if there is a winner
+        private bool IsThereWinner()
+        {
+            // Check if first horizontal row is three in a row
+            if (uxButton1.Text != ""
+                && uxButton1.Text == uxButton2.Text
+                && uxButton2.Text == uxButton3.Text)
+            {
+                uxToolStripStatusLabel.Text = string.Format("{0} is a winner", uxButton1.Text);
+                return true;
+            }
+            return false;
+        }
+
+        private void menuFileNew_Click(object sender, EventArgs e)
+        {
+            ResetBoard();
+        }
+
+        private void menuFileExit_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private void uxButton_Click(object sender, EventArgs e)
+        {
+            // Get the current "button" that was clicked.
+            // The actual "button" is a Label control.
+            var button = (Label)sender;
+
+            // Check if the button is empty
+            if (string.IsNullOrEmpty(button.Text))
+            {
+                button.Text = currentTurn;
+
+                if (IsThereWinner())
+                {
+                    DisableBoard();
+                }
+                else
+                {
+                    if (currentTurn == "X")
+                    {
+                        currentTurn = "O";
+                    }
+                    else
+                    {
+                        currentTurn = "X";
+                    }
+
+                    WhoseTurn();
+                }
+            }
+        }
+    }
+}
